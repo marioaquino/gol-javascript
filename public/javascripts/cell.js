b9c0eb98db92fe1d81ac3f9ae7a47aaf3f69bf06
Matthew Taylor <rhyolight@gmail.com>
2010-11-01 08:14:04 -0700

    detached cell functions from instances and put them on prototype

--- public/javascripts/cell.js
+++ public/javascripts/cell.js
@@ -1,66 +1,75 @@
 var gol = gol || {};
 
-gol.Cell = function(myIndex, grid){
+(function() {
 
-  var neighbors = function() {
-    var cols = grid.columns();
-    var rows = grid.rows();
-    var upRow = myIndex - cols;
-    var downRow = myIndex + cols;
-    var myColumn = myIndex % cols;
-    var bounds = [upRow   - 1, upRow,   upRow   + 1,
-                  myIndex - 1, /* me */ myIndex + 1,
-                  downRow - 1, downRow, downRow + 1].filter(function(index) {
-                    //in negative space?
-                    if (index < 0) {
-                      return false;
-                    }
+	  function neighbors() {
+	    var cols = this.grid.columns();
+		var self = this;
+	    var rows = this.grid.rows();
+	    var upRow = this.myIndex - cols;
+	    var downRow = this.myIndex + cols;
+	    var myColumn = this.myIndex % cols;
+	    var bounds = [upRow   - 1, upRow,   upRow   + 1,
+	                  this.myIndex - 1, /* me */ this.myIndex + 1,
+	                  downRow - 1, downRow, downRow + 1].filter(function(index) {
+	                    //in negative space?
+	                    if (index < 0) {
+	                      return false;
+	                    }
 
-                    //is x coord outside the grid?
-                    var row = Math.floor(index / cols);
-                    if (row < 0 || row >= rows) {
-                      return false;
-                    }
+	                    //is x coord outside the grid?
+	                    var row = Math.floor(index / cols);
+	                    if (row < 0 || row >= rows) {
+	                      return false;
+	                    }
 
-                    var column = index % cols;
-                    //More than one column away from each other
-                    if (Math.abs(column - myColumn) > 1) {
-                      return false;
-                    }
+	                    var column = index % cols;
+	                    //More than one column away from each other
+	                    if (Math.abs(column - myColumn) > 1) {
+	                      return false;
+	                    }
 
-                    return true;
-                  });
+	                    return true;
+	                  });
 
-    return bounds.map(function(neighborIndex) {
-      return grid[neighborIndex];
-    });
-  };
+	    return bounds.map(function(neighborIndex) {
+	      return self.grid[neighborIndex];
+	    });
+	  };
 
-  var status = false;
 
-  this.alive = function() {
-    return status;
-  };
+	gol.Cell = function(myIndex, grid){
+		this.myIndex = myIndex;
+		this.grid = grid;
+	  this.status = false;
 
-  this.evolve = function() {
-    var liveNeighborsTotal = neighbors().reduce(function(total, cell) {
-      return total + (cell.alive() ? 1 : 0);
-    }, 0);
-    var that = this;
-    return function() {
-      if (liveNeighborsTotal === 3) {
-        that.revive();
-      } else {
-        that.kill();
-      }
-    };
-  };
-  
-  this.revive = function() {
-    status = true;
-  };
-  
-  this.kill = function() {
-    status = false;
-  };
-};
+	};
+
+	gol.Cell.prototype.alive = function() {
+	  return this.status;
+	};
+
+	gol.Cell.prototype.evolve = function() {
+	  var liveNeighborsTotal = neighbors.call(this).reduce(function(total, cell) {
+	    return total + (cell.alive() ? 1 : 0);
+	  }, 0);
+	  var that = this;
+	  return function() {
+	    if (liveNeighborsTotal === 3) {
+	      that.revive();
+	    } else {
+	      that.kill();
+	    }
+	  };
+	};
+
+	gol.Cell.prototype.revive = function() {
+	  this.status = true;
+	};
+
+	gol.Cell.prototype.kill = function() {
+	  this.status = false;
+	};
+	
+}());
+
